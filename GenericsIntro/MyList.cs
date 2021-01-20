3d225cda28b8ec4a18a50ba1aaf7092ddce41f39
Ersin <ersinakyuz47@gmail.com>
2021-01-20 23:51:59 +0300

    4.Gün

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;  //bunu yazıp alta ctor yaz ve tab la
+        public MyList()
+        {   
+            items = new T[0];    
+        }
+
+        public void Add(T item)
+        {
+
+            //Dizinin eleman sayısı 0 old.için burda 1 artırıyoruz
+            T[] tempArray = items; // önceki değerler kaybolmasın diye gecici adrese taşıdık temp ile
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+
+            }
+            items[items.Length - 1] = item;
+
+
+        }
+            
+    }
+}
---
+++ Oop1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop1
+{
+    class ProductManager
+    {
+        //crud işlemleri manager da yap
+
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi");
+
+
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+
+        }
+        
+    }
+}
