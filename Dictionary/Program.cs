25cddc6781d983dc9199c166b2b61fab72f5bcc0
Ersin <ersinakyuz47@gmail.com>
2021-01-23 20:00:12 +0300

    MyDictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                MyDictionary<string, string> names = new MyDictionary<string, string>();
+
+                names.Add("1", "Mehmet");
+                names.Add("2", "Ceyhan");
+                names.Add("3", "Deniz");
+                names.Add("4", "Erkan");
+
+            names.GetAll();
+            Console.ReadLine();
+            }
+        
+    }
+}
